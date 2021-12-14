ba90961aa681bdd264f4919900801a6320767720
Stas Vavich <someonesp@inbox.ru>
2021-12-14 13:52:14 -0800

    0.0

--- demo/main.cpp
+++ demo/main.cpp
@@ -1,5 +1,23 @@
-#include <example.hpp>
+// Copyright by NIK
+
+#include <iostream>
+
+#include "ptr.hpp"
+
 
 int main() {
-  example();
+  std::cout << "Test!"<<"\n";
+
+  int *p = new int{5};
+
+
+  SharedPtr sh1(p);
+
+  std::cout << sh1.use_count() << std::endl;
+
+  SharedPtr<int> sh2(sh1);
+
+  std::cout << sh2.use_count() << std::endl;
+
+  return 0;
 }
